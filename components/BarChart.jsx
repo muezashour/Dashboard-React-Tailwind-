544b7f68310ce4c2f1c8531bf9857771a92e1095
muez ashour <muazashour@gmail.com>
2025-09-18 13:48:35 +0300

    first commit

---
+++ components/BarChart.jsx
@@ -0,0 +1,69 @@
+import React, {useState , useEffect} from 'react'
+import { Bar } from 'react-chartjs-2';
+
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js'
+
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+const BarChart = () => {
+    const [chartData, setChartData] = useState({
+        datasets:[],
+    });
+    const [chartOptions, setChartOptions] = useState({});
+
+    useEffect(() => {
+        setChartData({
+            labels: ['Mon','Tues','Wed','Thurs','Fri','Sat', 'Sun'],
+            datasets: [
+                {
+                    label: 'Sales $',
+                    data: [12833, 43618, 17386, 37733, 27499, 84793, 57378],
+                    borderColor: 'rgb(53,162,235)',
+                    backgroundColor:'rgba(53,162,235,0.4)'
+                },
+            ],
+
+        })
+        setChartOptions({
+            plugins: {
+                legend: {
+                    position:'top',
+                },
+                title: {
+                    display: true,
+                    text:'Daily Revenue'
+                }
+            },
+            maintainAspectRatio: false,
+            responsive: true
+
+        })
+
+    },[])
+
+return (
+    <>
+        <div className="w-full md:col-span-2 relative lg:h-[50vh] m-auto p-4 rounded-lg border-gray-50 bg-white">
+            <Bar data={chartData} options={chartOptions} />
+        </div>
+    </>
+)
+}
+
+export default BarChart
---
+++ components/ResentOrders.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { data } from "../data/data";
+import { FaShoppingBag } from "react-icons/fa";
+
+const ResentOrders = () => {
+  return (
+    <div className="w-full col-span-1 relative lg:h-[70vh] h-[50vh] p-4 m-auto rounded-lg border-gray-50 bg-white overflow-scroll">
+          <h1>Recenet Orders </h1>
+          <ul>
+              {data.map((order, id) => (
+                  <li key={id} className="bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-3 flex items-center cursor-pointer">
+                      <div className="bg-purple-100 rounded-lg p-3 mr-2"><FaShoppingBag className="text-purple-800" />
+                      </div>
+                      <div className="pl-4 ">
+                          <p className="text-gray-800 font-bold"> ${order.total}</p>
+                          <p className="text-gray-400 text-sm">{order.name.first }</p>
+                      </div>
+                      <p className="lg:flex md:hidden absolute right-6 text-sm">{order.date}</p>
+
+                </li>
+            ))}
+          </ul>
+    </div>
+  );
+};
+
+export default ResentOrders;
